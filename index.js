91ac0eca9fa07404794f2cfc5051b14a0572157d
+ <+>
2023-07-31 14:02:30 -0600

    commit lab one missed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+let freesample = 'This one is on me!'
+  if (feet <= 400){
+return freesample; 
+  }else if (feet > 2500){
+    return "No can do.";
+  } else if (feet > 400 && feet <= 2000){
+   return "That will be twenty bucks.";
+} else if (feet > 2000 && feet <= 2500){
+  return "I will gladly take your thirty bucks.";
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ 
+   if (city === "NYC"){
+    return "Ok, sounds good.";
+   } else {
+    return "No go.";
+   }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(value){
   // Write your code here!
+ switch (value) {
+  case 'generous':
+    return 'Thank you so much.';
+    case 'regular':
+      return 'Thank you.';
+      default: 
+        return 'Bye.';
+ }
 }
